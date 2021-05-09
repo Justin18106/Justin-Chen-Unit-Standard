92a0af500a5cedac725e4b362795f75e41e2019f
Justin18106 <81192065+Justin18106@users.noreply.github.com>
2021-05-09 17:28:15 +1200

    Add files via upload

--- Justin Chen Unit Standard.py
+++ Justin Chen Unit Standard.py
@@ -1,31 +1,50 @@
 import math
 
 file = open('tunnel times fuller set.csv')
-data_file = file.read()
-data_file = data_file.replace(',', '\n')
-data_list = data_file.split()
-print(data_list)
+datafile = file.read()
+datafile = datafile.replace(',', '\n')
+datalist = datafile.split()
 
-east_camera = 185
-west_camera = 105
+x = 5
+y = -1
+fine = [30, 80, 120, 170, 230, 300, 400, 510, 630]
+speedfine = {1:30, 2:30, 3:30, 4:30, 5:30}
+for i in range(9):
+    y = y+1
+    for i in range(5):
+        x = x+1
+        speedfine[x] = fine[y]
 
-for i in range(0, len(data_list)//2, 2):
-    duplicate = [a for a, val in enumerate(data_list) if val == data_list[i]] #pairs the duplicate number plates in a list
+eastcamera = 185
+westcamera = 105
 
-    enter = data_list[duplicate[0]+1]
+speedlimit = int(input('What is the speed limit? '))
+
+for i in range(0, len(datalist)//2, 2):
+    duplicate = [a for a, val in enumerate(datalist) if val == datalist[i]] #pairs the duplicate number plates in a list
+
+    enter = datalist[duplicate[0]+1]
     enter = enter.split(':')
     for i in range(len(enter)):
         enter[i] = int(enter[i])
-    enter_sec = (enter[0]*3600)+(enter[1]*60)+(enter[2])
+    entersecond = (enter[0]*3600)+(enter[1]*60)+(enter[2])
 
-    exit = data_list[duplicate[1]+1]
+    exit = datalist[duplicate[1]+1]
     exit = exit.split(':')
     for i in range(len(exit)):
         exit[i] = int(exit[i])
-    exit_sec = (exit[0]*3600)+(exit[1]*60)+(exit[2])
+    exitsecond = (exit[0]*3600)+(exit[1]*60)+(exit[2])
 
-    travel_time = exit_sec - enter_sec
-    speed_ms = (2400+east_camera+west_camera)/travel_time
-    speed_kmh = speed_ms*3.6
-    speed_kmh = math.floor(speed_kmh)
-    print(speed_kmh)
+    traveltime = exitsecond - entersecond
+    speedms = (2400+eastcamera+westcamera)/traveltime
+    speedkmh = speedms*3.6
+    speedkmh = math.floor(speedkmh)
+
+    overlimit = speedkmh - speedlimit
+    if overlimit > 0:
+        print('Carplate:', datalist[duplicate[0]])
+        print('Speed:', speedkmh, 'km/h')        
+        if overlimit <= 50:
+            print('Fine: $', speedfine[overlimit], '\n')
+        else:
+            print('Fine: $', fine[8],'\n')
