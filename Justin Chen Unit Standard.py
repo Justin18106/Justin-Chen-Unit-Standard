e2f5849dd632de441978e6800767dc41a5f3d649
Justin18106 <81192065+Justin18106@users.noreply.github.com>
2021-05-09 17:31:37 +1200

    Add files via upload

--- Justin Chen Unit Standard.py
+++ Justin Chen Unit Standard.py
@@ -47,49 +47,55 @@ def speed():
     speedlimit = input('\nWhat is the speed limit? (km/h): ')
     validate(speedlimit)
     speedlimit = int(y)
-    for i in range(0, len(datalist)//2, 2):  # repeats the code for every second element in datalist
-        duplicate = [a for a, val in enumerate(datalist) if val == datalist[i]]  # pairs the duplicate number plates in a list
-        if len(duplicate) > 1:
+    try:
+        for i in range(0, len(datalist), 2):  # repeats the code for every second element in datalist
+            duplicate = [a for a, val in enumerate(datalist) if val == datalist[i]]  # pairs the duplicate number plates in a list
+            if len(duplicate) > 1:
 
-            duplicateindex = [x for x in range(len(datalist)) if datalist[x] == datalist[duplicate[0]]]
+                duplicateindex = [x for x in range(len(datalist)) if datalist[x] == datalist[duplicate[0]]]
 
-            enter = datalist[duplicate[0]+1]  # finds the values after the first element in the pair
-            enter = enter.split(':')
-            for i in range(len(enter)):  # converts the values of the time into integers
-                enter[i] = int(enter[i])
-            entersecond = (enter[0]*3600)+(enter[1]*60)+(enter[2])  # converts the enter time into seconds
+                enter = datalist[duplicate[0]+1]  # finds the values after the first element in the pair
+                enter = enter.split(':')
+                for i in range(len(enter)):  # converts the values of the time into integers
+                    enter[i] = int(enter[i])
+                entersecond = (enter[0]*3600)+(enter[1]*60)+(enter[2])  # converts the enter time into seconds
 
-            exit = datalist[duplicate[1]+1]  # finds the values after the second element in the pair
-            exit = exit.split(':')
-            for i in range(len(exit)):
-                exit[i] = int(exit[i])
-            exitsecond = (exit[0]*3600)+(exit[1]*60)+(exit[2])
+                exit = datalist[duplicate[1]+1]  # finds the values after the second element in the pair
+                exit = exit.split(':')
+                for i in range(len(exit)):
+                    exit[i] = int(exit[i])
+                exitsecond = (exit[0]*3600)+(exit[1]*60)+(exit[2])
 
-            traveltime = exitsecond - entersecond
-            if traveltime > 240:  # add cars that take over 4 minutes to exit the tunnel to a list
-                fourminutes.append(datalist[duplicate[0]])
+                traveltime = exitsecond - entersecond
+                if traveltime > 240:  # add cars that take over 4 minutes to exit the tunnel to a list
+                    fourminutes.append(datalist[duplicate[0]])
 
-            try:  # calculates the average speed of the car
-                speedms = (2400+eastcamera+westcamera)/traveltime
-            except:
-                continue
+                try:  # calculates the average speed of the car
+                    speedms = (2400+eastcamera+westcamera)/traveltime
+                except:
+                    continue
 
-            speedkmh = speedms*3.6  # converts the speed to kilometers per hour
-            speedkmh = math.floor(speedkmh)  # rounds the speed down
+                speedkmh = speedms*3.6  # converts the speed to kilometers per hour
+                speedkmh = math.floor(speedkmh)  # rounds the speed down
 
-            overlimit = speedkmh - speedlimit
-            if overlimit > 0:  # prints data if speed is over limit
-                print('\nCarplate:', datalist[duplicate[0]])
-                print('Enter Time:', datalist[duplicate[0]+1])
-                print('Exit Time:', datalist[duplicate[1]+1])
-                print('Speed:', speedkmh, 'km/h')
-                if overlimit <= 50:
-                    print('Fine: $', speedfine[overlimit])
-                else:
-                    print('Fine: $', fine[8])
-        else:
-            once.append(datalist[duplicate[0]])
+                overlimit = speedkmh - speedlimit
+                if overlimit > 0:  # prints data if speed is over limit
+                    print('\nCarplate:', datalist[duplicate[0]])
+                    print('Enter Time:', datalist[duplicate[0]+1])
+                    print('Exit Time:', datalist[duplicate[1]+1])
+                    print('Speed:', speedkmh, 'km/h')
+                    if overlimit <= 50:
+                        print('Fine: $', speedfine[overlimit])
+                    else:
+                        print('Fine: $', fine[8])
 
+                datalist.pop(duplicateindex[1]+1)
+                datalist.pop(duplicateindex[1])
+
+            else:
+                once.append(datalist[duplicate[0]])
+    except:
+        pass
 
     print('\nThese cars have not exited within 4 minutes:\n', fourminutes)
     print('\nThese cars have no exited the tunnel:\n', once)
